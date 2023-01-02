d275997c353ab5ca257b999c360ab0fc9ba417a9
Fatih <h.fatih_@outlook.com>
2023-01-02 17:09:03 +0300

    counter removed

--- src/App.js
+++ src/App.js
@@ -9,14 +9,12 @@ import ChangePasswordPage from './pages/profile/ChangePasswordPage';
 import UserPreferencesPage from './pages/profile/UserPreferencesPage'
 import TransactionGroupDetail from './pages/TransactionGroupDetail';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Counter } from './features/user/Counter';
 
 function App() {
     return (
         <Router>
             <Routes >
                 <Route exact path='/' element={<HomePage />} />
-                <Route exact path='/counter' element={<Counter />} />
                 <Route exact path='/login' element={<LoginPage />} />
                 <Route exact path='/register' element={<Register />} />
                 <Route exact path='/profile' element={<ProfilePage />} />
