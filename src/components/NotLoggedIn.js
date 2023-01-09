81f46a4e6b5915b38cb1c369631f33ee2db5f2d7
Fatih <h.fatih_@outlook.com>
2023-01-09 15:52:49 +0300

    not logged in update

--- src/components/NotLoggedIn.js
+++ src/components/NotLoggedIn.js
@@ -7,10 +7,10 @@ function NotLoggedIn() {
             flexDirection: 'column',
         }}>
 
-            <div class="alert alert-warning" style={{ fontSize: 40 }}>
+            <div class="alert alert-warning" style={{ fontSize: 16, }}>
                 <strong >Warning!</strong> You are not logged in to see this content.
             </div>
-            <button onClick={() => window.location.href = '/login'} style={{ width: '25vw' }} type="button" class="btn btn-primary btn-block">Login</button>
+            <button onClick={() => window.location.href = '/login'} style={{ width: '20vw' }} type="button" class="btn btn-primary btn-block">Login</button>
             <button onClick={() => window.location.href = '/register'} style={{ width: '15vw', marginTop: 12 }} type="button" class="btn btn-warning btn-block">Register</button>
 
         </div>
