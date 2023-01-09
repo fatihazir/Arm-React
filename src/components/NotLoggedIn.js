2d3830a2cce2494dcb638ff19e27504eec4c44b3
Fatih <h.fatih_@outlook.com>
2023-01-09 15:38:49 +0300

    register button added

--- src/components/NotLoggedIn.js
+++ src/components/NotLoggedIn.js
@@ -11,6 +11,7 @@ function NotLoggedIn() {
                 <strong >Warning!</strong> You are not logged in to see this content.
             </div>
             <button onClick={() => window.location.href = '/login'} style={{ width: '20vw' }} type="button" class="btn btn-primary btn-block">Login</button>
+            <button onClick={() => window.location.href = '/register'} style={{ width: '20vw', marginTop: 12 }} type="button" class="btn btn-warning btn-block">Register</button>
 
         </div>
     )
