b279c9a0e49829672a6a20398b1c5c063a2fec41
Fatih <h.fatih_@outlook.com>
2023-01-09 15:37:50 +0300

    sidebar not logged in ui

--- src/common/sidebar.js
+++ src/common/sidebar.js
@@ -24,39 +24,29 @@ function Sidebar() {
             </Link>
         </div>
 
-        <PerfectScrollbar className="sidebar-items">
-            <ul className="list-unstyled ps-0">
-                <li className="mb-1">
-                    <Link tag="a" className="" to="/">
-                        <i className="fa fa-dashboard"></i> Associations
-                    </Link>
-                </li>
-                {/* <li className="mb-1">
-                    <Link tag="a" className="" to="/blank-page">
-                        <i className="fa fa-file-o"></i> Blank Page
-                    </Link>
-                </li>
-                <li className="border-top my-3"></li>
-                <li className="mb-1">
-                    <Link tag="a" className="" to="/typography">
-                        <i className="fa fa-text-width" aria-hidden="true"></i> Typography
-                    </Link>
-                </li> */}
+        {user.id && <>
+            <PerfectScrollbar className="sidebar-items">
+                <ul className="list-unstyled ps-0">
+                    <li className="mb-1">
+                        <Link tag="a" className="" to="/">
+                            <i className="fa fa-dashboard"></i> Associations
+                        </Link>
+                    </li>
+                </ul>
+            </PerfectScrollbar>
 
-            </ul>
-        </PerfectScrollbar>
-
-        <div className="dropdown fixed-bottom-dropdown">
-            <a href="#" className="d-flex align-items-center text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                <i className="fa fa-user-circle" aria-hidden="true"></i>
-                <span style={{ marginLeft: 12 }}>{user.firstName} {user.lastName}</span>
-            </a>
-            <ul className="dropdown-menu text-small shadow text-center" aria-labelledby="dropdownUser2">
-                <li> <li><button onClick={null} type="button" class="btn btn-primary btn-lg btn-block"><i className="fa fa-user-circle" aria-hidden="true"></i> Profile</button></li></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><button onClick={SignOut} type="button" class="btn btn-primary btn-lg btn-block"><i className="fa fa-sign-out" aria-hidden="true">Sign out</i></button></li>
-            </ul>
-        </div>
+            <div className="dropdown fixed-bottom-dropdown">
+                <a href="#" className="d-flex align-items-center text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i className="fa fa-user-circle" aria-hidden="true"></i>
+                    <span style={{ marginLeft: 12 }}>{user.firstName} {user.lastName}</span>
+                </a>
+                <ul className="dropdown-menu text-small shadow text-center" aria-labelledby="dropdownUser2">
+                    <li> <li><button onClick={null} type="button" class="btn btn-primary btn-lg btn-block"><i className="fa fa-user-circle" aria-hidden="true"></i> Profile</button></li></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><button onClick={SignOut} type="button" class="btn btn-primary btn-lg btn-block"><i className="fa fa-sign-out" aria-hidden="true">Sign out</i></button></li>
+                </ul>
+            </div>
+        </>}
     </div>
 
 
