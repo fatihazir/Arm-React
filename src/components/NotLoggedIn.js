79dee21abe114b848829ea392196a76374e1ad60
Fatih <h.fatih_@outlook.com>
2023-01-09 15:39:24 +0300

    login and register button widths

--- src/components/NotLoggedIn.js
+++ src/components/NotLoggedIn.js
@@ -10,8 +10,8 @@ function NotLoggedIn() {
             <div class="alert alert-warning" style={{ fontSize: 40 }}>
                 <strong >Warning!</strong> You are not logged in to see this content.
             </div>
-            <button onClick={() => window.location.href = '/login'} style={{ width: '20vw' }} type="button" class="btn btn-primary btn-block">Login</button>
-            <button onClick={() => window.location.href = '/register'} style={{ width: '20vw', marginTop: 12 }} type="button" class="btn btn-warning btn-block">Register</button>
+            <button onClick={() => window.location.href = '/login'} style={{ width: '25vw' }} type="button" class="btn btn-primary btn-block">Login</button>
+            <button onClick={() => window.location.href = '/register'} style={{ width: '15vw', marginTop: 12 }} type="button" class="btn btn-warning btn-block">Register</button>
 
         </div>
     )
